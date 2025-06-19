c03eb3a1b194abdaae28cd35199c48710d70e8ec
Elias2710 <65856695+Elias2710@users.noreply.github.com>
2025-06-19 08:00:50 +0700

    Delete migrations directory

--- migrations/20250603063326_add_status_translation_request/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "TranslationRequestStatus" AS ENUM ('REQUESTING', 'COMPLETED');
-
--- AlterTable
-ALTER TABLE "TranslationRequest" ADD COLUMN     "status" "TranslationRequestStatus" NOT NULL DEFAULT 'REQUESTING';
